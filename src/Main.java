c2637e942c9d7c1ab813006d7376fefcd0bcb7af
Antonio Hollerman <toniohollerprof@gmail.com>
2024-01-30 21:44:00 -0500

    empty main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        String FILE_NAME = "src\\goalsDB.txt";
-        Path pathToFile = Path.of(System.getProperty("user.dir"), FILE_NAME);
-        try{
-            List<String> lines = Files.readAllLines(pathToFile);
-            for (String line : lines){
-                System.out.println(line);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+        
     }
 }
