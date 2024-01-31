20af2862974b74058f3c283ee99d02693a35c00d
AntonioHollerman <toniohollerProf@gmail.com>
2024-01-31 17:01:26 -0500

    Finish first version of InputManagement

--- src/InputManagement.java
+++ src/InputManagement.java
@@ -1,15 +1,28 @@
+import java.util.Set;
+
 public class InputManagement {
     private final DbManager manager;
     public InputManagement(DbManager manager){
         this.manager = manager;
     }
     public void addCourse(){
-
+        System.out.println("Add a new course to study");
+        String courseName = GetInput.getCourseName();
+        double courseHours = GetInput.getCourseHours();
+        manager.addCourse(courseName, courseHours);
     }
     public void updateCourse(){
-
+        Set<String> coursesNames = manager.getCoursesNames();
+        String[] namesInArray = coursesNames.toArray(new String[0]);
+        int choiceIndex = GetInput.selectOption(namesInArray);
+        double hours = GetInput.getHoursCompleted("How many hours completed for " + namesInArray[choiceIndex] +
+                ": ");
+        manager.editCourse(namesInArray[choiceIndex], hours);
     }
     public void removeCourse(){
-
+        Set<String> coursesNames = manager.getCoursesNames();
+        String[] namesInArray = coursesNames.toArray(new String[0]);
+        int choiceIndex = GetInput.selectOption(namesInArray);
+        manager.removeCourse(namesInArray[choiceIndex]);
     }
 }
